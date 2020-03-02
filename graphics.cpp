62b71980f36724853a0b0f9ece4753d55e4cd3b9
ahrifatarafat <57371182+ahrifatarafat@users.noreply.github.com>
2020-03-02 23:31:58 +0600

    Add files via upload

--- graphics.cpp
+++ graphics.cpp
@@ -3,20 +3,20 @@
 #include<string.h>
 using namespace std;
 void setbkcolor(int color);
-int graph(int x,int y)
+/*int graph(int x,int y)
 {
     int p;
 
     return p;
-}
+}*/
 void interfac()
 {
 
 
     int gd = DETECT,gm;
     initgraph(&gd,&gm," ");
-    setcolor(RED);
-    for(int i = 20; i<=380; i+=40)
+    setcolor(RED);/*line gular color*/
+    for(int i = 20; i<=380; i+=40)/*ghor akanor jonno ertuku*/
     {
         line(i,20,i,380);
     }
@@ -25,10 +25,10 @@ void interfac()
         line(20,i,380,i);
     }
 
-    setbkcolor(GREEN);
+    setbkcolor(GREEN);/*font er color ar cursor move er color sobuj hoibo*/
 
 }
-
+/*outtexxy diya graphics window er x,y sthanankke print kore*/
 int main()
 {
     int cursor;
@@ -38,69 +38,70 @@ int main()
     char input;
     int p;
     interfac();
-    outtextxy(40,40,"*");
+    outtextxy(40,40,"    ");
     int x=30,y=30;
     while(scanf("%c",&input)!=EOF)
     {
 
-        if(input == 'a')
+        if(input == 'a')        /*cursor e bame jaoar jonno*/
         {
             x = x-40;
-            outtextxy(x,y,"*");
+            outtextxy(x,y,"    ");
         }
-        else if(input == 's')
+        else if(input == 's')/*corspr e niche jaoar jonno*/
         {
             y = y + 40;
-            outtextxy(x,y,"*");
+            outtextxy(x,y,"    ");
         }
-        else if(input == 'd')
+        else if(input == 'd')/*cursor dane neoar jonno*/
         {
             x = x + 40;
-            outtextxy(x,y,"*");
+            outtextxy(x,y,"    ");
         }
-        else if(input == 'w')
+        else if(input == 'w') /*cursor upore jaoar jonno*/
         {
             y = y - 40;
-            outtextxy(x,y,"*");
+            outtextxy(x,y,"    ");
         }
 
-        if(input == 1)
+        else if(input == 1)   /*for game input*/
         {
-            outtextxy(x,y,"1");
+            setcolor(RED);
+            outtextxy(x,y," 1");
         }
         else if(input == 2)
         {
-            outtextxy(x,y,"2");
+            outtextxy(x,y," 2");
         }
         else if(input == 3)
         {
-            outtextxy(x,y,"3");
+            outtextxy(x,y," 3");
         }
         else if(input == 4)
         {
-            outtextxy(x,y,"4");
+            outtextxy(x,y," 4");
         }
         else if(input == 5)
         {
-            outtextxy(x,y,"5");
+            outtextxy(x,y," 5");
         }
         else if(input == 6)
         {
-            outtextxy(x,y,"6");
+            outtextxy(x,y," 6");
         }
         else if(input == 7)
         {
-            outtextxy(x,y,"7");
+            outtextxy(x,y," 7");
         }
         else if(input == 8)
         {
-            outtextxy(x,y,"8");
+            outtextxy(x,y," 8");
         }
         else if(input == 9)
         {
-            outtextxy(x,y,"9");
+            outtextxy(x,y," 9");
         }
-        while(input != 'a' && input != 's' && input != 'd' && input != 'w' && input <'1' && input >9)
+        while(input != 'a' && input != 's' && input != 'd' && input != 'w' && input <='1' && input >='9') /*jodi input vul hoy*/
         {
             outtextxy(550,500,"Invalid Input");
             scanf("%c",&input);
